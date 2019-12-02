9b94c0c5fe0868d887d293213db5194c3f437c51
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2019-12-02 16:08:09 +0300

    Add developer scripts generation

--- scripts/build.py
+++ scripts/build.py
@@ -78,6 +78,7 @@ def build_sdk_builder( directory ):
 def build_js_develop( root_dir ):
   _run_npm(root_dir + "/sdkjs/build")
   _run_grunt(root_dir + "/sdkjs/build", ["--level=WHITESPACE_ONLY", "--formatting=PRETTY_PRINT"] + base.sdkjs_addons_param())
+  _run_grunt(root_dir + "/sdkjs/build", ["develop"] + base.sdkjs_addons_param())
   _run_npm(root_dir + "/web-apps/build")
   _run_npm(root_dir + "/web-apps/build/sprites")
   _run_grunt(root_dir + "/web-apps/build/sprites", [])

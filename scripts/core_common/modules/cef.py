ca56b57c0f5c130760971253b70c15e27756a00d
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-02-19 19:07:48 +0300

    refactoring cef fetch

--- scripts/core_common/modules/cef.py
+++ scripts/core_common/modules/cef.py
@@ -15,7 +15,7 @@ def make():
 
   platforms = ["win_64", "win_32", "winxp_64", "winxp_32", "linux_64", "linux_32", "mac_64"]
 
-  url = "http://d2ettrnqo7v976.cloudfront.net/cef/"
+  url = "http://d2ettrnqo7v976.cloudfront.net/cef/3770/"
 
   for platform in platforms:
     if not config.check_option("platform", platform):
@@ -26,36 +26,51 @@ def make():
       if ("winxp_32" != platform) and not config.check_option("platform", "win_32_xp"):
         continue
 
-    if base.is_dir(platform + "/build"):
+    if (0 == platform.find("mac")):
+      url += "mac"
+    else:
+      url += platform
+
+    url += "/cef_binary.7z"
+
+    if not base.is_dir(platform):
+      base.create_dir(platform)
+
+    os.chdir(platform)
+
+    data_url = base.get_file_last_modified_url(url)
+    old_data_url = base.readFile("./cef_binary.7z.data")
+
+    if (old_cur != old_data_url):
+      if base.is_file("./cef_binary.7z"):
+        base.delete_file("./cef_binary.7z")
+      if base.is_dir("build"):
+        base.delete_dir("build")
+
+    if base.is_dir("build"):
       continue
 
-    base.create_dir(platform)
     # download
-
-    url_addon = "3163/" + platform
-    if (0 == platform.find("linux")):
-      url_addon = "3202/" + platform
-    elif (0 == platform.find("mac")):
-      url_addon = "3163/mac"
-
-    if not base.is_file(platform + "/cef_binary.7z"):
-      base.download(url + url_addon + "/cef_binary.7z", platform + "/cef_binary.7z")
+    if not base.is_file("./cef_binary.7z"):
+      base.download(url, "./cef_binary.7z")
 
     # extract
-    base.extract(platform + "/cef_binary.7z", platform)
+    base.extract("./cef_binary.7z", "./")
 
-    base.create_dir(platform + "/build")
+    base.create_dir("./build")
 
     # deploy
     if ("mac_64" != platform):
-      base.copy_files(platform + "/cef_binary/Release/*", platform + "/build/")
-      base.copy_files(platform + "/cef_binary/Resources/*", platform + "/build/")
+      base.copy_files("cef_binary/Release/*", "build/")
+      base.copy_files("cef_binary/Resources/*", "build/")
 
     if (0 == platform.find("linux")):
-      base.cmd("chmod", ["a+xr", platform + "/build/locales"])
+      base.cmd("chmod", ["a+xr", "build/locales"])
 
     if ("mac_64" == platform):
-      base.cmd("mv", [platform + "/cef_binary", platform + "/build/Chromium Embedded Framework.framework"])
+      base.cmd("mv", ["cef_binary", "build/Chromium Embedded Framework.framework"])
+
+    os.chdir(base_dir)
 
   os.chdir(old_cur)
   return

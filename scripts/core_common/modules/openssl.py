e6cb5fb7cb5502dda9f8721a354bceefda113310
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2020-03-03 19:09:06 +0300

    Add params to openssl build

--- scripts/core_common/modules/openssl.py
+++ scripts/core_common/modules/openssl.py
@@ -16,12 +16,28 @@ def make():
   old_cur = os.getcwd()
   os.chdir(base_dir)
 
+  openssl_version_good = "openssl_version_1"
+  openssl_version = base.readFile("./openssl.data")
+  if (openssl_version != openssl_version_good):
+    base.delete_file("./openssl.data")
+    base.writeFile("./openssl.data", openssl_version_good)
+    if base.is_dir("openssl"):
+      base.delete_dir("openssl")
+
   if not base.is_dir("openssl"):
     base.cmd("git", ["clone", "--depth=1", "https://github.com/openssl/openssl.git"])
 
   os.chdir(base_dir + "/openssl")
   if not base.is_file("Makefile"):
     base.cmd("./config", ["no-shared", "no-asm"])
+
+  if base.is_file("./libssl.a") and base.is_file("./libcrypto.a"):
+    os.chdir(old_cur)
+    return    
+
+  if ("linux" == base.host_platform()):
+    replaceInFile("./Makefile", "CFLAGS=-Wall -O3", "CFLAGS=-Wall -O3 -fvisibility=hidden")
+    replaceInFile("./Makefile", "CXXFLAGS=-Wall -O3", "CXXFLAGS=-Wall -O3 -fvisibility=hidden")
 
   base.cmd("make", ["build_libs"])
 

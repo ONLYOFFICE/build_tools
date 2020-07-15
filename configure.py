7d1e6e8c9a7bd5f83fd534b27cb3b88120660f8d
hellonadya <n.knyazeva118@gmail.com>
2020-07-15 16:56:05 +0300

    Update configure.py
    - add 1 === true
              0 === false

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ import optparse
 arguments = sys.argv[1:]
 
 parser = optparse.OptionParser()
-parser.add_option("--update", action="store", type="string", dest="update", default="1", help="defines whether it's necessary to update/clone repos. If it's set to true, build_tools automatically get the necessary subrepos. If it's set to false, you should define which ones to use")
+parser.add_option("--update", action="store", type="string", dest="update", default="1", help="defines whether it's necessary to update/clone repos. If it's set to true (1 === true), build_tools automatically get the necessary subrepos. If it's set to false (0 === false), you should define which ones to use")
 parser.add_option("--update-light", action="store", type="string", dest="update-light", default="", help="performs pull/clone without switching branches, can be used only if update is true.")
 parser.add_option("--branch", action="store", type="string", dest="branch", default="master", help="branch/tag name, used only if update is true and update_light is not used. Updates/clones all the repos and switches the branch to the proper one deleting all the local changes")
 parser.add_option("--clean", action="store", type="string", dest="clean", default="1", help="defines whether to build everything anew (or changed  c/c++ sources only).")

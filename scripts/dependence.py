f144f286ca4e96c5332985717290622966d963a9
Alexander Trofimov <Alexander.Trofimov@onlyoffice.com>
2020-10-08 11:16:15 +0300

    [check] Add get_programUninstalls

--- scripts/dependence.py
+++ scripts/dependence.py
@@ -2,7 +2,34 @@ import sys
 import os
 import base
 
+if (sys.version_info[0] >= 3):
+  import winreg
+else:
+  import _winreg as winreg
+
 def check_pythonPath():
   path = base.get_env('PATH')
   if (path.find(sys.exec_prefix) == -1):
     base.set_env('PATH', sys.exec_prefix + os.pathsep + path)
+
+def get_programUninstallsByFlag(sName, flag):
+  info = []
+  aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+  aKey= winreg.OpenKey(aReg, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, winreg.KEY_READ | flag)
+  count_subkey = winreg.QueryInfoKey(aKey)[0]
+
+  for i in range(count_subkey):
+    try:
+      asubkey_name = winreg.EnumKey(aKey, i)
+      asubkey = winreg.OpenKey(aKey, asubkey_name)
+      progName = winreg.QueryValueEx(asubkey, 'DisplayName')[0]
+    
+      if (progName.find(sName) != -1):
+        info.append(winreg.QueryValueEx(asubkey, 'UninstallString')[0])
+      
+    except:
+      pass
+      
+  return info
+def get_programUninstalls(sName):
+  return get_programUninstallsByFlag(sName, winreg.KEY_WOW64_32KEY) + get_programUninstallsByFlag(sName, winreg.KEY_WOW64_64KEY)

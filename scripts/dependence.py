90da8d975cc8b8663f6ebb54ca754bfcd275ceb9
Alexander Trofimov <Alexander.Trofimov@onlyoffice.com>
2020-10-08 11:38:15 +0300

    [check] Add uninstall

--- scripts/dependence.py
+++ scripts/dependence.py
@@ -20,28 +20,6 @@ class CDependencies:
     self.pathsToRemove.update(oCdependencies.pathsToRemove)
     self.pathToValidMySQLServer = oCdependencies.pathToValidMySQLServer
 
-def get_programUninstallsByFlag(sName, flag):
-  info = []
-  aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-  aKey= winreg.OpenKey(aReg, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, winreg.KEY_READ | flag)
-  count_subkey = winreg.QueryInfoKey(aKey)[0]
-
-  for i in range(count_subkey):
-    try:
-      asubkey_name = winreg.EnumKey(aKey, i)
-      asubkey = winreg.OpenKey(aKey, asubkey_name)
-      progName = winreg.QueryValueEx(asubkey, 'DisplayName')[0]
-    
-      if (progName.find(sName) != -1):
-        info.append(winreg.QueryValueEx(asubkey, 'UninstallString')[0])
-      
-    except:
-      pass
-      
-  return info
-def get_programUninstalls(sName):
-  return get_programUninstallsByFlag(sName, winreg.KEY_WOW64_32KEY) + get_programUninstallsByFlag(sName, winreg.KEY_WOW64_64KEY)
-
 def check_pythonPath():
   path = base.get_env('PATH')
   if (path.find(sys.exec_prefix) == -1):
@@ -166,3 +144,42 @@ def check_mysqlInstaller():
     pass
   dependence.progsToInstall.add('MySQLInstaller')
   return dependence
+
+def get_programUninstallsByFlag(sName, flag):
+  info = []
+  aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+  aKey= winreg.OpenKey(aReg, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall", 0, winreg.KEY_READ | flag)
+  count_subkey = winreg.QueryInfoKey(aKey)[0]
+
+  for i in range(count_subkey):
+    try:
+      asubkey_name = winreg.EnumKey(aKey, i)
+      asubkey = winreg.OpenKey(aKey, asubkey_name)
+      progName = winreg.QueryValueEx(asubkey, 'DisplayName')[0]
+    
+      if (progName.find(sName) != -1):
+        info.append(winreg.QueryValueEx(asubkey, 'UninstallString')[0])
+      
+    except:
+      pass
+      
+  return info
+def get_programUninstalls(sName):
+  return get_programUninstallsByFlag(sName, winreg.KEY_WOW64_32KEY) + get_programUninstallsByFlag(sName, winreg.KEY_WOW64_64KEY)
+
+def uninstallProgram(sName):
+  print("Uninstalling " + sName + "...")
+  
+  unInfo = get_programUninstalls(sName)
+  for info in unInfo:
+    if (base.is_file(info) == False):
+      info = info.replace('/I', '/x').replace('/i', '/x')
+    else:
+      info = '"' + info + '" /S'
+      
+    print(info)  
+    if (os.system(info) != 0):
+      print("Uninstalling was failed!")
+      return False
+      
+  return True

2e2cf4e9dd27c711c72c43ca695d4cfc13386f65
KhromovNikita <Nikita.Khromov@onlyoffice.com>
2020-11-17 13:18:29 +0300

    Fix

--- scripts/build_js.py
+++ scripts/build_js.py
@@ -106,10 +106,14 @@ def build_sdk_native(directory):
 
 def build_js_develop(root_dir):
   #_run_npm_cli(root_dir + "/sdkjs/build")
-  _run_npm(root_dir + "/sdkjs/build")
-  _run_grunt(root_dir + "/sdkjs/build", get_build_param(False) + base.sdkjs_addons_param())
-  _run_grunt(root_dir + "/sdkjs/build", ["develop"] + base.sdkjs_addons_param())
-  _run_npm(root_dir + "/web-apps/build")
-  _run_npm(root_dir + "/web-apps/build/sprites")
-  _run_grunt(root_dir + "/web-apps/build/sprites", [])
+  plug_in_folders = config.option("plug-in folders")
+  if (plug_in_folders != ""):
+    plug_in_folders = "/" + plug_in_folders
+  
+  _run_npm(root_dir + plug_in_folders + "/sdkjs/build")
+  _run_grunt(root_dir + plug_in_folders + "/sdkjs/build", get_build_param(False) + base.sdkjs_addons_param())
+  _run_grunt(root_dir + plug_in_folders + "/sdkjs/build", ["develop"] + base.sdkjs_addons_param())
+  _run_npm(root_dir + plug_in_folders + "/web-apps/build")
+  _run_npm(root_dir + plug_in_folders + "/web-apps/build/sprites")
+  _run_grunt(root_dir + plug_in_folders + "/web-apps/build/sprites", [])
   return

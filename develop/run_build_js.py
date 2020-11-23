cec8c17d9df33bdd87b36400de4ee81ecef4e4d8
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2020-11-23 20:17:38 +0300

    Fix

--- develop/run_build_js.py
+++ develop/run_build_js.py
@@ -5,9 +5,7 @@ import config
 import base
 import sys
 
-branch = base.run_command('git rev-parse --abbrev-ref HEAD')['stdout']
-
-base.cmd_in_dir('../', 'python', ['configure.py', '--branch', branch or 'develop', '--develop', '1', '--module', 'server', '--update', '0', '--update-light', '0', '--clean', '0'])
+base.cmd_in_dir('../', 'python', ['configure.py'])
 config.parse()
 
 build_js.build_js_develop(sys.argv[1])

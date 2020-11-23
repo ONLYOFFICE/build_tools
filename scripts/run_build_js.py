69284ae0b9c5d5d73e1660045a0553fba67a2a69
KhromovNikita <Nikita.Khromov@onlyoffice.com>
2020-11-23 17:30:10 +0300

    Fix

--- scripts/run_build_js.py
+++ scripts/run_build_js.py
@@ -3,9 +3,10 @@
 import build_js
 import config
 import base
+import sys
 
 branch = base.run_command('git rev-parse --abbrev-ref HEAD')['stdout']
 
 config.parse()
 base.cmd_in_dir('../', 'python', ['configure.py', '--branch', branch or 'develop', '--develop', '1', '--module', 'server', '--update', '0', '--update-light', '0', '--clean', '0'])
-build_js.build_js_develop('/var/www/onlyoffice/documentserver')
+build_js.build_js_develop(sys.argv[1])

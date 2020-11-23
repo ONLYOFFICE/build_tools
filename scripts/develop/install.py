20907e7cab4070b08e6d9785c2ab245b6185a1a4
Alexander Trofimov <Alexander.Trofimov@onlyoffice.com>
2020-11-23 14:40:02 +0300

    [run] Delete unused

--- scripts/develop/install.py
+++ scripts/develop/install.py
@@ -13,7 +13,6 @@ parser = optparse.OptionParser()
 parser.add_option("--install", action="append", type="string", dest="install", default=[], help="provides install dependencies")
 parser.add_option("--uninstall", action="append", type="string", dest="uninstall", default=[], help="provides uninstall dependencies")
 parser.add_option("--remove-path", action="append", type="string", dest="remove-path", default=[], help="provides path dependencies to remove")
-parser.add_option("--mysql-path", action="store", type="string", dest="mysql-path", default="", help="provides path to mysql")
 
 (options, args) = parser.parse_args(arguments)
 configOptions = vars(options)

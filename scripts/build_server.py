673ef5a905552233ba947cc33fe8f8b6eba51cfb
Roman Demidov <roman.demidov@onlyoffice.com>
2020-12-07 13:45:01 +0300

    Add changes

--- scripts/build_server.py
+++ scripts/build_server.py
@@ -57,9 +57,10 @@ def make():
   example_dir = base.get_script_dir() + "/../../document-server-integration/web/documentserver-example/nodejs"
   base.delete_dir(example_dir  + "/node_modules")
   base.cmd_in_dir(example_dir, "npm", ["install"])
-  sync_rpc_lib_dir = example_dir + "/node_modules/sync-rpc/lib"
-  patch_file = base.get_script_dir() + "/../tools/linux/sync-rpc.patch"
-  base.cmd_in_dir(sync_rpc_lib_dir, "patch", ["-N", "-i", patch_file])
+  if ("linux" == base.host_platform()):
+    sync_rpc_lib_dir = example_dir + "/node_modules/sync-rpc/lib"
+    patch_file = base.get_script_dir() + "/../tools/linux/sync-rpc.patch"
+    base.cmd_in_dir(sync_rpc_lib_dir, "patch", ["-N", "-i", patch_file])
   base.cmd_in_dir(example_dir, "pkg", [".", "-t", pkg_target, "-o", "example"])
 
 def build_server_develop():

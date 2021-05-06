e0754402d83c01f89af75e85db128d2bb9f6fdd6
Vladimir Gorshenkov <vladimir.gorshenkov@onlyoffice.com>
2021-05-06 18:25:39 +0300

    [scripts][websocket] add general build script for all platform

--- scripts/core_common/modules/websocket.py
+++ scripts/core_common/modules/websocket.py
@@ -4,14 +4,14 @@ import sys
 sys.path.append('../..')
 import config
 import base
-import ixwebsocket
+import ixwebsocket_all
 import socketrocket
 
 config_file = base.get_script_dir() + "/../../core/Common/WebSocket/websocket.pri"
 
 def make():
   
-  ixwebsocket.make()
+  ixwebsocket_all.make()
   socketrocket.make()
 
   if (-1 != config.option("websocketlib").find("ixwebsocket")):

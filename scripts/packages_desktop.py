909ea2f4e0174008191cef62f6460d5d786f205e
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2021-05-19 18:22:05 +0300

    Copy changes html for appcast

--- scripts/packages_desktop.py
+++ scripts/packages_desktop.py
@@ -23,6 +23,7 @@ def make():
       update_dir = macos_dir + "/build/update"
       version_zip = re.sub(r"\.(\d+)$", "", base.get_env("PRODUCT_VERSION"))
       macos_zip = macos_dir + "/build/ONLYOFFICE-" + version_zip + ".zip"
+      changes_dir = macos_dir + "/ONLYOFFICE/update/updates/ONLYOFFICE/changes/" + version_zip
 
       base.cmd_in_dir(macos_dir, "bundler", ["exec", "fastlane", "release", "skip_git_bump:true"])
 
@@ -31,6 +32,12 @@ def make():
       base.create_dir(update_dir)
       base.copy_dir_content(base.get_env("UPDATE_STORAGE"), update_dir, ".zip")
       base.copy_file(macos_zip, update_dir)
+      for file in os.listdir(update_dir):
+        if file.endswith(".zip"):
+          base.copy_file(changes_dir + "/ReleaseNotes.html",
+            update_dir + "/" + os.path.splitext(file)[0] + ".html")
+          base.copy_file(changes_dir + "/ReleaseNotesRU.html",
+            update_dir + "/" + os.path.splitext(file)[0] + ".ru.html")
       base.cmd(macos_dir + "/Vendor/Sparkle/bin/generate_appcast", [update_dir])
       base.cmd_in_dir(update_dir, "find", [".", "-type", "f", "-name", "*.zip",
         "-not", "-name", "*-" + version_zip + ".zip", "-delete"])

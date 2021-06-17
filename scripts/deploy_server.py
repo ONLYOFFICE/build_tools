2383b77413fd47ae0986f84767d1aaba12a16a36
Roman Demidov <roman.demidov@onlyoffice.com>
2021-06-17 11:36:56 +0300

    Add changes

--- scripts/deploy_server.py
+++ scripts/deploy_server.py
@@ -57,11 +57,6 @@ def make():
     base.create_dir(build_server_dir + '/Metrics/node_modules/modern-syslog/build/Release')
     base.copy_file(bin_server_dir + "/Metrics/node_modules/modern-syslog/build/Release/core.node", build_server_dir + "/Metrics/node_modules/modern-syslog/build/Release/core.node")
 
-    #base.create_dir(build_server_dir + '/SpellChecker')
-    #base.copy_exe(bin_server_dir + "/SpellChecker", build_server_dir + '/SpellChecker', "spellchecker")
-    #base.create_dir(build_server_dir + '/SpellChecker/node_modules/nodehun/build/Release')
-    #base.copy_file(bin_server_dir + "/SpellChecker/node_modules/nodehun/build/Release/Nodehun.node", build_server_dir + '/SpellChecker/node_modules/nodehun/build/Release/Nodehun.node')
-    
 
     qt_dir = base.qt_setup(native_platform)
     platform = native_platform
@@ -139,7 +134,6 @@ def make():
       branding_dir = git_dir + '/' + config.option("branding") + '/server'
 
     #dictionaries
-    #spellchecker_dictionaries = build_server_dir + '/SpellChecker/dictionaries'
     spellchecker_dictionaries = build_server_dir + '/dictionaries'
     spellchecker_dictionaries_files = server_dir + '/../dictionaries/*_*'
     base.create_dir(spellchecker_dictionaries)
@@ -201,10 +195,6 @@ def make():
       base.copy_dir(bin_server_dir + '/FileConverter/node_modules', root_dir_snap + '/server/FileConverter/node_modules')
       base.copy_dir(bin_server_dir + '/FileConverter/sources', root_dir_snap + '/server/FileConverter/sources')
       base.delete_file(root_dir_snap + '/server/FileConverter/converter')
-      #base.copy_dir(bin_server_dir + '/SpellChecker/node_modules', root_dir_snap + '/server/SpellChecker/node_modules')
-      #base.copy_dir(bin_server_dir + '/SpellChecker/sources', root_dir_snap + '/server/SpellChecker/sources')
-      #base.delete_file(root_dir_snap + '/server/SpellChecker/spellchecker')
-      #base.copy_dir(bin_server_dir + '/dictionaries', root_dir_snap + '/server/dictionaries')
       base.copy_dir(bin_server_dir + '/Common/node_modules', root_dir_snap + '/server/Common/node_modules')
       base.copy_dir(bin_server_dir + '/Common/sources', root_dir_snap + '/server/Common/sources')
       base.copy_dir(bin_example_dir + '/..', root_dir_snap + '/example')

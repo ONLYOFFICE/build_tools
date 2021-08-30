cec0423b1445dc179d1685a16ca8e6cc8d4a1440
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2021-08-30 11:34:19 +0300

    Support cef mac_arm64 version

--- scripts/core_common/modules/cef.py
+++ scripts/core_common/modules/cef.py
@@ -13,7 +13,7 @@ def make():
   old_cur = os.getcwd()
   os.chdir(base_dir)
 
-  platforms = ["win_64", "win_32", "win_64_xp", "win_32_xp", "linux_64", "linux_32", "mac_64"]
+  platforms = ["win_64", "win_32", "win_64_xp", "win_32_xp", "linux_64", "linux_32", "mac_64", "mac_arm64"]
 
   url = "http://d2ettrnqo7v976.cloudfront.net/cef/4280/"
 

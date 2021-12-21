964570de22e96e9cd72d3556ca6b4e49b84f2032
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2021-12-21 18:26:05 +0300

    Refactoring desktop packages

--- scripts/packages_desktop.py
+++ scripts/packages_desktop.py
@@ -17,115 +17,19 @@ git_dir = base.get_script_dir() + "/../.."
 package_dir = os.path.abspath(git_dir + "/desktop-apps")
 
 def make(platform, targets):
-
   if ("windows" == platform):
     make_windows(targets)
-
-  for target in targets:
-
-    # macOS
-    elif ("macos" == platform):
-
-      macos_dir = package_dir + "/macos"
-      update_dir = macos_dir + "/build/update"
-      changes_dir = macos_dir + "/ONLYOFFICE/update/updates/ONLYOFFICE/changes"
-
-      if (-1 != target.find("diskimage")):
-
-        if (target == "diskimage-x86_64"):
-          lane = "release_x86_64"
-          scheme = "ONLYOFFICE-x86_64"
-        elif (target == "diskimage-v8-x86_64"):
-          lane = "release_v8"
-          scheme = "ONLYOFFICE-v8"
-        elif (target == "diskimage-arm64"):
-          lane = "release_arm"
-          scheme = "ONLYOFFICE-arm"
-        else:
-          exit(1)
-
-        print("Build package " + scheme)
-
-        print("$ bundler exec fastlane " + lane + " skip_git_bump:true")
-        base.cmd_in_dir(macos_dir, "bundler", ["exec", "fastlane", lane, "skip_git_bump:true"])
-
-      if (-1 != target.find("sparkle-updates")):
-
-        print("Build updates")
-
-        app_version = base.run_command("/usr/libexec/PlistBuddy -c 'print :CFBundleShortVersionString' " +
-          macos_dir + "/build/ONLYOFFICE.app/Contents/Info.plist")['stdout']
-        zip_filename = scheme + "-" + app_version
-        macos_zip = macos_dir + "/build/" + zip_filename + ".zip"
-        update_storage_dir = base.get_env("ARCHIVES_DIR") + "/" + scheme + "/_updates"
-
-        base.create_dir(update_dir)
-        base.copy_dir_content(update_storage_dir, update_dir, ".zip")
-        base.copy_dir_content(update_storage_dir, update_dir, ".html")
-        base.copy_file(macos_zip, update_dir)
-
-        notes_src = changes_dir + "/" + app_version + "/ReleaseNotes.html"
-        notes_dst = update_dir + "/" + zip_filename + ".html"
-        cur_date = base.run_command("LC_ALL=en_US.UTF-8 date -u \"+%B %e, %Y\"")['stdout']
-        if base.is_exist(notes_src):
-          base.copy_file(notes_src, notes_dst)
-          base.replaceInFileRE(notes_dst,
-            r"(<span class=\"releasedate\">).+(</span>)", "\\1 - " + cur_date + "\\2")
-        else:
-          base.writeFile(notes_dst, "placeholder\n")
-
-        notes_src = changes_dir + "/" + app_version + "/ReleaseNotesRU.html"
-        notes_dst = update_dir + "/" + zip_filename + ".ru.html"
-        cur_date = base.run_command("LC_ALL=ru_RU.UTF-8 date -u \"+%e %B %Y\"")['stdout']
-        if base.is_exist(notes_src):
-          base.copy_file(notes_src, notes_dst)
-          base.replaceInFileRE(notes_dst,
-            r"(<span class=\"releasedate\">).+(</span>)", "\\1 - " + cur_date + "\\2")
-        else:
-          base.writeFile(notes_dst, "placeholder\n")
-
-        print("$ ./generate_appcast " + update_dir)
-        base.cmd(macos_dir + "/Vendor/Sparkle/bin/generate_appcast", [update_dir])
-
-        print("Edit Sparkle appcast links")
-
-        sparkle_base_url = "https://download.onlyoffice.com/install/desktop/editors/mac"
-        if (target == "diskimage-x86_64"):      sparkle_base_url += "/x86_64"
-        elif (target == "diskimage-v8-x86_64"): sparkle_base_url += "/v8"
-        elif (target == "diskimage-arm64"):     sparkle_base_url += "/arm"
-
-        base.replaceInFileRE(update_dir + "/onlyoffice.xml",
-          r"(<sparkle:releaseNotesLink>)(?:.+ONLYOFFICE-(?:x86|x86_64|v8|arm)-([0-9.]+)\..+)(</sparkle:releaseNotesLink>)",
-          "\\1" + sparkle_base_url + "/updates/changes/\\2/ReleaseNotes.html\\3")
-        base.replaceInFileRE(update_dir + "/onlyoffice.xml",
-          r"(<sparkle:releaseNotesLink xml:lang=\"ru\">)(?:ONLYOFFICE-(?:x86|x86_64|v8|arm)-([0-9.]+)\..+)(</sparkle:releaseNotesLink>)",
-          "\\1" + sparkle_base_url + "/updates/changes/\\2/ReleaseNotesRU.html\\3")
-        base.replaceInFileRE(update_dir + "/onlyoffice.xml",
-          r"(url=\")(?:.+/)(ONLYOFFICE.+\")", "\\1" + sparkle_base_url + "/updates/\\2")
-
-        print("Delete unnecessary files")
-
-        for file in os.listdir(update_dir):
-          if (-1 == file.find(app_version)) and (file.endswith(".zip") or file.endswith(".html")):
-            base.delete_file(update_dir + "/" + file)
-
-    # Linux
-    elif ("linux" == platform):
-
+  elif ("macos" == platform):
+    make_macos(targets)
+  elif ("linux" == platform):
+    if ("packages" in targets):
       build_dir = package_dir + "/win-linux/package/linux"
-
-      if (target == "packages"):
-
-        print("Clean")
-        base.cmd_in_dir(build_dir, "make", ["clean"])
-
-        print("Build packages")
-        base.cmd_in_dir(build_dir, "make", ["packages"])
-
-    else:
-
-      exit(1)
-
+      print("Clean")
+      base.cmd_in_dir(build_dir, "make", ["clean"])
+      print("Build packages")
+      base.cmd_in_dir(build_dir, "make", ["packages"])
+  else:
+    exit(1)
   return
 
 def make_windows(targets):
@@ -244,5 +148,93 @@ def make_windows(targets):
     if (-1 != target.find("portable")):
       print("Build package: portable")
       base.cmd_in_dir(package_dir, "7z", ["a", "-y", portable_zip_file, prefix_dir + "/*"])
+
+  return
+
+def make_macos(targets):
+  for target in targets:
+
+    macos_dir = package_dir + "/macos"
+    update_dir = macos_dir + "/build/update"
+    changes_dir = macos_dir + "/ONLYOFFICE/update/updates/ONLYOFFICE/changes"
+
+    if (-1 != target.find("diskimage")):
+
+      if (target == "diskimage-x86_64"):
+        lane = "release_x86_64"
+        scheme = "ONLYOFFICE-x86_64"
+      elif (target == "diskimage-v8-x86_64"):
+        lane = "release_v8"
+        scheme = "ONLYOFFICE-v8"
+      elif (target == "diskimage-arm64"):
+        lane = "release_arm"
+        scheme = "ONLYOFFICE-arm"
+      else:
+        exit(1)
+
+      print("Build package " + scheme)
+
+      print("$ bundler exec fastlane " + lane + " skip_git_bump:true")
+      base.cmd_in_dir(macos_dir, "bundler", ["exec", "fastlane", lane, "skip_git_bump:true"])
+
+    if (-1 != target.find("diskimage")) and (-1 != target.find("sparkle-updates")):
+
+      print("Build updates")
+
+      app_version = base.run_command("/usr/libexec/PlistBuddy -c 'print :CFBundleShortVersionString' " +
+        macos_dir + "/build/ONLYOFFICE.app/Contents/Info.plist")['stdout']
+      zip_filename = scheme + "-" + app_version
+      macos_zip = macos_dir + "/build/" + zip_filename + ".zip"
+      update_storage_dir = base.get_env("ARCHIVES_DIR") + "/" + scheme + "/_updates"
+
+      base.create_dir(update_dir)
+      base.copy_dir_content(update_storage_dir, update_dir, ".zip")
+      base.copy_dir_content(update_storage_dir, update_dir, ".html")
+      base.copy_file(macos_zip, update_dir)
+
+      notes_src = changes_dir + "/" + app_version + "/ReleaseNotes.html"
+      notes_dst = update_dir + "/" + zip_filename + ".html"
+      cur_date = base.run_command("LC_ALL=en_US.UTF-8 date -u \"+%B %e, %Y\"")['stdout']
+      if base.is_exist(notes_src):
+        base.copy_file(notes_src, notes_dst)
+        base.replaceInFileRE(notes_dst,
+          r"(<span class=\"releasedate\">).+(</span>)", "\\1 - " + cur_date + "\\2")
+      else:
+        base.writeFile(notes_dst, "placeholder\n")
+
+      notes_src = changes_dir + "/" + app_version + "/ReleaseNotesRU.html"
+      notes_dst = update_dir + "/" + zip_filename + ".ru.html"
+      cur_date = base.run_command("LC_ALL=ru_RU.UTF-8 date -u \"+%e %B %Y\"")['stdout']
+      if base.is_exist(notes_src):
+        base.copy_file(notes_src, notes_dst)
+        base.replaceInFileRE(notes_dst,
+          r"(<span class=\"releasedate\">).+(</span>)", "\\1 - " + cur_date + "\\2")
+      else:
+        base.writeFile(notes_dst, "placeholder\n")
+
+      print("$ ./generate_appcast " + update_dir)
+      base.cmd(macos_dir + "/Vendor/Sparkle/bin/generate_appcast", [update_dir])
+
+      print("Edit Sparkle appcast links")
+
+      sparkle_base_url = "https://download.onlyoffice.com/install/desktop/editors/mac"
+      if (target == "diskimage-x86_64"):      sparkle_base_url += "/x86_64"
+      elif (target == "diskimage-v8-x86_64"): sparkle_base_url += "/v8"
+      elif (target == "diskimage-arm64"):     sparkle_base_url += "/arm"
+
+      base.replaceInFileRE(update_dir + "/onlyoffice.xml",
+        r"(<sparkle:releaseNotesLink>)(?:.+ONLYOFFICE-(?:x86|x86_64|v8|arm)-([0-9.]+)\..+)(</sparkle:releaseNotesLink>)",
+        "\\1" + sparkle_base_url + "/updates/changes/\\2/ReleaseNotes.html\\3")
+      base.replaceInFileRE(update_dir + "/onlyoffice.xml",
+        r"(<sparkle:releaseNotesLink xml:lang=\"ru\">)(?:ONLYOFFICE-(?:x86|x86_64|v8|arm)-([0-9.]+)\..+)(</sparkle:releaseNotesLink>)",
+        "\\1" + sparkle_base_url + "/updates/changes/\\2/ReleaseNotesRU.html\\3")
+      base.replaceInFileRE(update_dir + "/onlyoffice.xml",
+        r"(url=\")(?:.+/)(ONLYOFFICE.+\")", "\\1" + sparkle_base_url + "/updates/\\2")
+
+      print("Delete unnecessary files")
+
+      for file in os.listdir(update_dir):
+        if (-1 == file.find(app_version)) and (file.endswith(".zip") or file.endswith(".html")):
+          base.delete_file(update_dir + "/" + file)
 
   return

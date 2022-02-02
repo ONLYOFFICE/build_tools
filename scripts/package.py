fb4f80589e734758ec510124ad46db7fb807f8b7
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2022-02-02 17:36:49 +0300

    Fix desktop packages build (#429)

--- scripts/package.py
+++ scripts/package.py
@@ -5,8 +5,8 @@ import package_server
 import package_builder
 
 def make(product):
-  if product == 'desktop': package_desktop.make()
-  if product == 'server':  package_server.make()
-  if product == 'builder': package_builder.make()
+  if   product == 'desktop': package_desktop.make()
+  elif product == 'server':  package_server.make()
+  elif product == 'builder': package_builder.make()
   else: exit(1)
   return

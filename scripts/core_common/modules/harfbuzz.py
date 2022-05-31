ae28bd21d6475198a97a9ddcd6382a3141bf5401
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2022-05-31 16:07:26 +0300

    Add new module

---
+++ scripts/core_common/modules/harfbuzz.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../../../scripts')
+import base
+import os
+
+def make():
+  print("[fetch & build]: harfbuzz")
+  base.cmd_in_dir(base.get_script_dir() + "/../../core/Common/3dParty/harfbuzz", "make.py")
+  return
+
+if __name__ == '__main__':
+  # manual compile
+  make()

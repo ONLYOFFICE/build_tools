d2faf5d42aae94cfdb89fb0b626857068e9738bb
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2022-10-18 16:52:58 +0300

    Add desktop help installer build / Fix bug 58536

--- scripts/package_desktop.py
+++ scripts/package_desktop.py
@@ -26,8 +26,9 @@ def make():
 #
 
 def make_windows():
-  global package_version, sign, machine, arch, xp, iscc_args, source_dir, \
-    innosetup_file, innosetup_update_file, advinst_file, portable_zip_file
+  global package_version, sign, machine, arch, xp, iscc_args, \
+    source_dir, source_help_dir, innosetup_file, innosetup_help_file, \
+    innosetup_update_file, advinst_file, portable_zip_file
 
   set_cwd(get_abspath(git_dir, build_dir))
 
@@ -58,6 +59,7 @@ def make_windows():
     suffix = arch + ("_xp" if xp else "")
     source_prefix = "win_" + machine + ("_xp" if xp else "")
     source_dir = get_path("%s/%s/%s/%s" % (out_dir, source_prefix, company_name_l, product_name_s))
+    source_help_dir = source_dir + "-help"
 
     if target.startswith('innosetup'):
       for year in vcredist_list:
@@ -72,6 +74,10 @@ def make_windows():
 
       if 'winsparkle-files' in targets:
         make_winsparkle_files()
+
+    if target.startswith('innosetup-help'):
+      innosetup_help_file = "%s_Help_%s_%s.exe" % (package_name, package_version, suffix)
+      make_innosetup_help()
 
     if target.startswith('advinst'):
       advinst_file = "%s_%s_%s.msi" % (package_name, package_version, suffix)
@@ -117,6 +123,29 @@ def make_innosetup():
     log("! file exist, skip")
     return
   cmd("iscc", iscc_args + ["common.iss"])
+  return
+
+def make_innosetup_help():
+  log("\n=== Build innosetup help project\n")
+  global iscc_args
+  iscc_args = [
+    "/Qp",
+    "/DsAppVersion=" + package_version,
+    "/DDEPLOY_PATH=" + source_help_dir,
+    "/D_ARCH=" + machine
+  ]
+  if onlyoffice:
+    iscc_args.append("/D_ONLYOFFICE=1")
+  else:
+    iscc_args.append("/DsBrandingFolder=" + get_abspath(git_dir, branding_dir))
+  if sign:
+    iscc_args.append("/DENABLE_SIGNING=1")
+    iscc_args.append("/Sbyparam=signtool.exe sign /v /n $q" + cert_name + "$q /t " + tsa_server + " $f")
+  log("--- " + innosetup_help_file)
+  if is_file(innosetup_help_file):
+    log("! file exist, skip")
+    return
+  cmd("iscc", iscc_args + ["help.iss"])
   return
 
 def make_innosetup_update():

315351662e054f14189908c8fe37b7c27f808962
Eugene Kozyrev <67453079+EugeneKozyrev@users.noreply.github.com>
2022-11-01 15:14:50 +0300

    Delete detect flag

--- scripts/package_desktop.py
+++ scripts/package_desktop.py
@@ -239,7 +239,7 @@ def make_advinst():
       "AddUpgradeCode '{47EEF706-B0E4-4C43-944B-E5F914B92B79}' \
                       -min_ver 7.1.1 -include_min_ver \
                       -max_ver 7.2.2 -include_max_ver\
-                      -include_lang 1049 -detect_only \
+                      -include_lang 1049 \
                       -property_name UPGRADE_2 -enable_migrate",
       "DelLanguage 1029 -buildname DefaultBuild",
       "DelLanguage 1031 -buildname DefaultBuild",

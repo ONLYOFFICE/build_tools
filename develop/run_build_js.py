e8ad53b990a8fc8ccc6ebde8721ae3fd222d5840
Sergey Konovalov <Sergey.Konovalov@onlyoffice.com>
2023-02-01 23:49:33 +0300

    [develop] Fix Dockerfile

--- develop/run_build_js.py
+++ develop/run_build_js.py
@@ -20,10 +20,10 @@ if base.is_exist(git_dir + "/server/FileConverter/bin/fonts.log"):
 # external server volume
 if base.is_exist(sys.argv[1] + '/server/DocService/package.json'):
   base.print_info('replace supervisor cfg to run docservice and converter from source')
-  base.replaceInFileRE("/etc/supervisor/conf.d/ds-docservice.conf", "command=.*", "command=" + "node " + git_dir + "/server/DocService/sources/server.js")
-  base.replaceInFileRE("/app/ds/setup/config/supervisor/ds/ds-docservice.conf", "command=.*", "command=" + "node " + git_dir + "/server/DocService/sources/server.js")
-  base.replaceInFileRE("/etc/supervisor/conf.d/ds-converter.conf", "command=.*", "command=" + "node " + git_dir + "/server/FileConverter/sources/convertermaster.js")
-  base.replaceInFileRE("/app/ds/setup/config/supervisor/ds/ds-converter.conf", "command=.*", "command=" + "node " + git_dir + "/server/FileConverter/sources/convertermaster.js")
+  base.replaceInFileRE("/etc/supervisor/conf.d/ds-docservice.conf", "command=.*", "command=node " + git_dir + "/server/DocService/sources/server.js")
+  base.replaceInFileRE("/app/ds/setup/config/supervisor/ds/ds-docservice.conf", "command=.*", "command=node " + git_dir + "/server/DocService/sources/server.js")
+  base.replaceInFileRE("/etc/supervisor/conf.d/ds-converter.conf", "command=.*", "command=node " + git_dir + "/server/FileConverter/sources/convertermaster.js")
+  base.replaceInFileRE("/app/ds/setup/config/supervisor/ds/ds-converter.conf", "command=.*", "command=node " + git_dir + "/server/FileConverter/sources/convertermaster.js")
   base.print_info('run_server.run_docker_server')
   run_server.run_docker_server();
 else:
@@ -33,8 +33,10 @@ else:
     base.generate_doctrenderer_config(git_dir + "/server/FileConverter/bin/DoctRenderer.config", "../../../sdkjs/deploy/", "server", "../../../web-apps/vendor/")
 
   base.print_info('replace supervisor cfg to run docservice and converter from pkg')
-  base.replaceInFileRE("/etc/supervisor/conf.d/ds-docservice.conf", "command=.*", "command=/var/www/onlyoffice/documentserver/server/DocService/docservice")
-  base.replaceInFileRE("/etc/supervisor/conf.d/ds-converter.conf", "command=.*", "command=/var/www/onlyoffice/documentserver/server/FileConverter/converter")
+  base.replaceInFileRE("/etc/supervisor/conf.d/ds-docservice.conf", "command=node .*", "command=/var/www/onlyoffice/documentserver/server/DocService/docservice")
+  base.replaceInFileRE("/app/ds/setup/config/supervisor/ds/ds-docservice.conf", "command=node .*", "command=/var/www/onlyoffice/documentserver/server/DocService/docservice")
+  base.replaceInFileRE("/etc/supervisor/conf.d/ds-converter.conf", "command=node .*", "command=/var/www/onlyoffice/documentserver/server/FileConverter/converter")
+  base.replaceInFileRE("/app/ds/setup/config/supervisor/ds/ds-converter.conf", "command=node .*", "command=/var/www/onlyoffice/documentserver/server/FileConverter/converter")
   base.print_info('build_js_develop: ' + git_dir)
   build_js.build_js_develop(git_dir)
   

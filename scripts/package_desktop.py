3f0385d4699318197f66e4b38213b56c2359ba51
Alexey Golubev <alexey.golubev@onlyoffice.com>
2023-07-03 18:58:15 +0500

    Change online-help url (#708)
    * Change online-help uri
    
    * Change online help url

--- scripts/package_desktop.py
+++ scripts/package_desktop.py
@@ -401,7 +401,7 @@ def make_macos():
   utils.sh('/usr/libexec/PlistBuddy -c "Set :CFBundleVersion %s" %s' \
       % (bundle_version, plist_path), verbose=True)
   utils.sh('/usr/libexec/PlistBuddy -c "Add :ASCWebappsHelpUrl string %s" %s' \
-      % ("https://download.onlyoffice.com/install/desktop/editors/help/v" + app_version + "-1/apps", plist_path), verbose=True)
+      % ("https://download.onlyoffice.com/install/desktop/editors/help/v" + app_version + "/apps", plist_path), verbose=True)
 
   utils.log("RELEASE=" + release_bundle_version_string + "(" + release_bundle_version + ")" \
         + "\nCURRENT=" + common.version + "(" + bundle_version + ")")

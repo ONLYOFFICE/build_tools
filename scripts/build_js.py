a0511ca3ac833175bbeac08113393c695b55745b
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2024-01-20 21:24:02 +0300

    Fix build js for native

--- scripts/build_js.py
+++ scripts/build_js.py
@@ -74,20 +74,28 @@ def make():
     base.create_dir(out_dir + "/mobile/sdkjs")
     vendor_dir_src = base_dir + "/../web-apps/vendor/"
     sdk_dir_src = base_dir + "/../sdkjs/deploy/sdkjs/"
+  
+    prefix_js = [
+      vendor_dir_src + "xregexp/xregexp-all-min.js", 
+      vendor_dir_src + "underscore/underscore-min.js",
+      base_dir + "/../sdkjs/common/Native/native.js",
+      base_dir + "/../sdkjs-native/common/common.js",
+      base_dir + "/../sdkjs/common/Native/jquery_native.js"
+    ]
 
-    base.join_scripts([vendor_dir_src + "xregexp/xregexp-all-min.js", 
-                   vendor_dir_src + "underscore/underscore-min.js",
-                   base_dir + "/../sdkjs/common/Native/native.js",
-                   base_dir + "/../sdkjs-native/common/common.js",
-                   base_dir + "/../sdkjs/common/Native/jquery_native.js"], 
-                   out_dir + "/mobile/sdkjs/banners.js")
+    postfix_js = [
+      base_dir + "/../sdkjs/common/libfont/engine/fonts_native.js",
+      base_dir + "/../sdkjs/common/Charts/ChartStyles.js"
+    ]
+
+    base.join_scripts(prefix_js, out_dir + "/mobile/sdkjs/banners.js")
 
     base.create_dir(out_dir + "/mobile/sdkjs/word")
-    base.join_scripts([out_dir + "/mobile/sdkjs/banners.js", sdk_dir_src + "word/sdk-all-min.js", sdk_dir_src + "word/sdk-all.js"], out_dir + "/mobile/sdkjs/word/script.bin")
+    base.join_scripts([out_dir + "/mobile/sdkjs/banners.js", sdk_dir_src + "word/sdk-all-min.js", sdk_dir_src + "word/sdk-all.js"] + postfix_js, out_dir + "/mobile/sdkjs/word/script.bin")
     base.create_dir(out_dir + "/mobile/sdkjs/cell")
-    base.join_scripts([out_dir + "/mobile/sdkjs/banners.js", sdk_dir_src + "cell/sdk-all-min.js", sdk_dir_src + "cell/sdk-all.js"], out_dir + "/mobile/sdkjs/cell/script.bin")
+    base.join_scripts([out_dir + "/mobile/sdkjs/banners.js", sdk_dir_src + "cell/sdk-all-min.js", sdk_dir_src + "cell/sdk-all.js"] + postfix_js, out_dir + "/mobile/sdkjs/cell/script.bin")
     base.create_dir(out_dir + "/mobile/sdkjs/slide")
-    base.join_scripts([out_dir + "/mobile/sdkjs/banners.js", sdk_dir_src + "slide/sdk-all-min.js", sdk_dir_src + "slide/sdk-all.js"], out_dir + "/mobile/sdkjs/slide/script.bin")
+    base.join_scripts([out_dir + "/mobile/sdkjs/banners.js", sdk_dir_src + "slide/sdk-all-min.js", sdk_dir_src + "slide/sdk-all.js"] + postfix_js, out_dir + "/mobile/sdkjs/slide/script.bin")
 
     base.delete_file(out_dir + "/mobile/sdkjs/banners.js")
   return

d1f401077d702111ea6ba2a7757e0db2ceb6664a
Jefri Abdullah <jefri0000@gmail.com>
2024-09-12 19:51:14 +0800

    add deb-builder

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
 ENV TZ=Etc/UTC
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
--- oo-build.sh
+++ oo-build.sh
@@ -1,12 +1,80 @@
 #!/usr/bin/env bash
 set -x
 
+#######################################################################
+# OnlyOffice Package Builder
+
+# Copyright (C) 2024 BTACTIC, SCCL
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+usage() {
+cat <<EOF
+
+  $0
+  Copyright BTACTIC, SCCL
+  Licensed under the GNU PUBLIC LICENSE 3.0
+
+  Usage: $0 --product-version=PRODUCT_VERSION --build-number=BUILD_NUMBER --unlimited-organization=ORGANIZATION --tag-suffix=-TAG_SUFFIX --debian-package-suffix=-DEBIAN_PACKAGE_SUFFIX
+  Example: $0 --product-version=7.4.1 --build-number=36 --unlimited-organization=btactic-oo --tag-suffix=-btactic --debian-package-suffix=-btactic
+
+  For Github actions you might want to either build only binaries or build only deb so that it's easier to prune containers
+  Example: $0 --product-version=7.4.1 --build-number=36 --unlimited-organization=btactic-oo --tag-suffix=-btactic --debian-package-suffix=-btactic --binaries-only
+  Example: $0 --product-version=7.4.1 --build-number=36 --unlimited-organization=btactic-oo --tag-suffix=-btactic --debian-package-suffix=-btactic --deb-only
+
+EOF
+
+}
+
+BINARIES_ONLY="false"
+DEB_ONLY="false"
+
+# Check the arguments.
+for option in "$@"; do
+  case "$option" in
+    -h | --help)
+      usage
+      exit 0
+    ;;
+    --binaries-only)
+      BINARIES_ONLY="true"
+    ;;
+    --deb-only)
+      DEB_ONLY="true"
+    ;;
+  esac
+done
+
 BUILD_BINARIES="true"
+BUILD_DEB="true"
+
+if [ ${BINARIES_ONLY} == "true" ] ; then
+  BUILD_BINARIES="true"
+  BUILD_DEB="false"
+fi
+
+if [ ${DEB_ONLY} == "true" ] ; then
+  BUILD_BINARIES="false"
+  BUILD_DEB="true"
+fi
 
 build_oo_binaries() {
 
-  _OUT_FOLDER=out
+  _OUT_FOLDER=$1
   _QT_FOLDER="tools/linux/qt_build"
+  _QT_SRC="tools/linux/qt-everywhere-opensource-src-5.9.9"
   _PRODUCT_VERSION=8.1.3
   _BUILD_NUMBER=3
   _TAG_SUFFIX=-raven
@@ -22,7 +90,7 @@ build_oo_binaries() {
   -v $(pwd)/${_QT_FOLDER}:/build_tools/tools/linux/qt_build \
   -v $(pwd)/${_OUT_FOLDER}:/build_tools/out \
   onlyoffice-document-editors-builder \
-  /bin/bash -c 'cd tools/linux && python3 ./build_qt.py && python ./automate.py --branch=tags/'"${_GIT_CLONE_BRANCH}"
+  /bin/bash -c 'cd tools/linux && python3 ./automate.py server --branch=tags/'"${_GIT_CLONE_BRANCH}"
   cd ..
 
 }
@@ -30,4 +98,29 @@ build_oo_binaries() {
 if [ "${BUILD_BINARIES}" == "true" ] ; then
   build_oo_binaries "out" "${PRODUCT_VERSION}" "${BUILD_NUMBER}"
   build_oo_binaries_exit_value=$?
+fi
+
+# Simulate that binaries build went ok
+# when we only want to make the deb
+if [ ${DEB_ONLY} == "true" ] ; then
+  build_oo_binaries_exit_value=0
+fi
+
+if [ "${BUILD_DEB}" == "true" ] ; then
+  if [ ${build_oo_binaries_exit_value} -eq 0 ] ; then
+    cd deb_build
+    docker build --tag onlyoffice-deb-builder . -f Dockerfile-deb-builder
+    docker run \
+      --env PRODUCT_VERSION=${PRODUCT_VERSION} \
+      --env BUILD_NUMBER=${BUILD_NUMBER} \
+      -v $(pwd):/usr/local/unlimited-onlyoffice-package-builder:ro \
+      -v $(pwd):/root:rw \
+      -v $(pwd)/../build_tools:/root/build_tools:ro \
+      onlyoffice-deb-builder /bin/bash -c "/usr/local/unlimited-onlyoffice-package-builder/onlyoffice-deb-builder.sh --product-version ${PRODUCT_VERSION} --build-number ${BUILD_NUMBER}"
+    cd ..
+  else
+    echo "Binaries build failed!"
+    echo "Aborting... !"
+    exit 1
+  fi
 fi

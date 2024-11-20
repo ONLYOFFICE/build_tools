02b4655a1645ba124a32feeca69ae54131be4294
Green <Kirill.Poljakov@onlyoffice.com>
2024-11-20 16:07:21 +0300

    Refactoring

--- scripts/core_common/modules/iwork.py
+++ scripts/core_common/modules/iwork.py
@@ -8,10 +8,16 @@ import os
 import subprocess
 
 def make(use_gperf = True):
-  base_dir = base.get_script_dir() + "/../../core/Common/3dParty/apple"
-  base.cmd_in_dir(base_dir, "python", ["fetch.py", str(use_gperf)])
-  return
+    base_dir = base.get_script_dir() + "/../../core/Common/3dParty/apple"
+
+    cmd_args = ["fetch.py"]
+
+    if True == use_gperf:
+        cmd_args.append("--gperf")
+
+    base.cmd_in_dir(base_dir, "python", cmd_args)
+    return
 
 if __name__ == '__main__':
-  # manual compile
-  make(False)
+    # manual compile
+    make(False)

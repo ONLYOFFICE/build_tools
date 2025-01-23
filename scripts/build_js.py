6a43b86912082dbd4ef348d9cae97b0b4b1901a5
maxkadushkin <maxim.kadushkin@onlyoffice.com>
2025-01-23 11:15:01 +0300

    [desktop] for bug 62528

--- scripts/build_js.py
+++ scripts/build_js.py
@@ -60,6 +60,10 @@ def make():
     deldirs = ['ie', 'mobile', 'embed']
     [base.delete_dir(root + "/" + d) for root, dirs, f in os.walk(out_dir + "/desktop/web-apps/apps") for d in dirs if d in deldirs]
 
+    # for bug 62528. remove empty folders
+    walklist = list(os.walk(out_dir + "/desktop/sdkjs"))
+    [base.delete_dir(p) for p, _, _ in walklist[::-1] if len(os.listdir(p)) == 0]
+
     base.copy_file(base_dir + "/../web-apps/apps/api/documents/index.html.desktop", out_dir + "/desktop/web-apps/apps/api/documents/index.html")
     
     build_interface(base_dir + "/../desktop-apps/common/loginpage/build")

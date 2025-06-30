64c32043ccdd106481c5dc06158673dde8e65b6e
Prokhorov Kirill <kirill.prohorov@onlyoffice.com>
2025-06-30 16:07:37 +0300

    Fix cmake build

--- scripts/core_common/modules/heif.py
+++ scripts/core_common/modules/heif.py
@@ -5,11 +5,12 @@ import os
 import config
 
 def make_x265(src_dir):
-    base.cmd("git", ["clone", "https://bitbucket.org/multicoreware/x265_git.git"])
+    if not base.is_dir("x265_git"):
+        base.cmd("git", ["clone", "https://bitbucket.org/multicoreware/x265_git.git"])
     build_dir = src_dir + "/x265_git/build"
     os.chdir(build_dir)
-    base.cmd("cmake", ["../source"])
-    base.cmd("make", ["../source"])
+    base.cmd("cmake", ["-G", "\"Visual Studio 16 2019\"", "-A", "x64", "../source"])
+    base.cmd("cmake", ["--build", ".", "--config", "Release"])
     os.chdir(src_dir)
     return
 
@@ -17,33 +18,23 @@ def make_de265(src_dir):
     if not base.is_dir("libde265"):
         base.cmd("git", ["clone", "https://github.com/strukturag/libde265.git"])
     os.chdir(src_dir + "/libde265")
-    if "windows" == base.host_platform():
-        tmp_bat = []
-        tmp_bat.append("call \"" + config.option("vs-path") + "/vcvarsall.bat\" x64")
-        tmp_bat.append("call nmake -f Makefile.vc7 /nologo /s /c")
-        tmp_bat.append("copy /y dec265\dec265.exe bin_x64\"")
-        tmp_bat.append("copy /y enc265\enc265.exe bin_x64\"")
-        tmp_bat.append("copy /y libde265\libde265.dll bin_x64\"")
-        tmp_bat.append("copy /y libde265\libde265.lib bin_x64\lib\"")
-        tmp_bat.append("copy /y libde265\libde265.exp bin_x64\lib\"")
-        base.run_as_bat(tmp_bat, True)
-    if "linux" == base.host_platform():
-        base.cmd("cmake", [".."])
-        base.cmd("make")
+    base.cmd("cmake", ["./", "-DCMAKE_BUILD_TYPE=Release"])
+    base.cmd("cmake", ["--build", ".", "--config", "Release"])
     os.chdir(src_dir)
     return
         
 
 def make_heif(src_dir):
-    base.cmd("git", ["clone", "https://github.com/strukturag/libheif.git"])
-    build_dir = src_dir + "/libheif/build"
-    base.create_dir(build_dir)
+    if not base.is_dir("libheif"):
+        base.cmd("git", ["clone", "https://github.com/strukturag/libheif.git"])
+    build_dir = src_dir + "/libheif/libheif/api/libheif"
     os.chdir(build_dir)
-    if "windows" == base.host_platform():
-
-    if "linux" == base.host_platform():
-        base.cmd("cmake", ["--preset=release", ".."])
-        base.cmd("make")
+    base.cmd("cmake", ["../../../", "-DCMAKE_TYPE_BUILD=Release", 
+             "-DLIBDE265_INCLUDE_DIR=\"" + src_dir + "/libde265\"",
+             "-DLIBDE265_LIBRARY=\"" + src_dir + "/libde265/libde265/Release/de265.lib\"",
+             "-DX265_INCLUDE_DIR=\"" + src_dir + "/x265_git/source\"",
+             "-DX265_LIBRARY=\"" + src_dir + "/x265_git/build/Release/libx265.lib\""])
+    base.cmd("cmake", ["--build", ".", "--config", "Release"])
     os.chdir(src_dir)
     return
 
@@ -55,7 +46,7 @@ def make():
     os.chdir(new_dir)
 
     if not base.is_dir("heif"):
-        #make_x265(new_dir)
+        make_x265(new_dir)
         make_de265(new_dir)
         make_heif(new_dir)
 

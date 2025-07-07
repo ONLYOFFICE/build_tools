9c80b95dbec0b71af7004dfe6481f7ee01cfce27
Prokhorov Kirill <kirill.prohorov@onlyoffice.com>
2025-07-07 18:00:44 +0300

    Add build for linux and mac

--- scripts/core_common/modules/heif.py
+++ scripts/core_common/modules/heif.py
@@ -9,8 +9,16 @@ def make_x265(src_dir):
         base.cmd("git", ["clone", "https://bitbucket.org/multicoreware/x265_git.git"])
     build_dir = src_dir + "/x265_git/build"
     os.chdir(build_dir)
-    base.cmd("cmake", ["-G", "Visual Studio 16 2019", "-A", "x64", "../source"])
-    base.cmd("cmake", ["--build", ".", "--config", "Release"])
+    if "windows" == base.host_platform():
+        base.cmd("cmake", ["-G", "Visual Studio 16 2019", "-A", "x64", "../source"])
+        base.cmd("cmake", ["--build", ".", "--config", "Release"])
+    if "linux" == base.host_platform():
+        base.cmd("cmake", ["-G", "Unix Makefiles", "../source"])
+        base.cmd("cmake", ["../source"])
+        base.cmd("make", ["-j$(nproc)"])
+    if "mac" == base.host_platform():
+        base.cmd("cmake", ["-G", "Unix Makefiles", "../source"])
+        base.cmd("make", ["-j$(sysctl -n hw.ncpu)"])
     base.copy_file(build_dir + "/x265_config.h", src_dir + "/x265_git/source")
     os.chdir(src_dir)
     return
@@ -20,7 +28,12 @@ def make_de265(src_dir):
         base.cmd("git", ["clone", "https://github.com/strukturag/libde265.git"])
     os.chdir(src_dir + "/libde265")
     base.cmd("cmake", ["./", "-DCMAKE_BUILD_TYPE=Release"])
-    base.cmd("cmake", ["--build", ".", "--config", "Release"])
+    if "windows" == base.host_platform():
+        base.cmd("cmake", ["--build", ".", "--config", "Release"])
+    if "linux" == base.host_platform():
+        base.cmd("make", ["-j$(nproc)"])
+    if "mac" == base.host_platform():
+        base.cmd("make", ["-j$(sysctl -n hw.ncpu)"])
     os.chdir(src_dir)
     return
         
@@ -30,12 +43,31 @@ def make_heif(src_dir):
         base.cmd("git", ["clone", "https://github.com/strukturag/libheif.git"])
     build_dir = src_dir + "/libheif/libheif/api/libheif"
     os.chdir(build_dir)
-    base.cmd("cmake", ["../../../", "-DCMAKE_TYPE_BUILD=Release", 
+    if "windows" == base.host_platform():
+        base.cmd("cmake", ["../../../", "-DCMAKE_BUILD_TYPE=Release", 
              "-DLIBDE265_INCLUDE_DIR=" + src_dir + "/libde265",
              "-DLIBDE265_LIBRARY=" + src_dir + "/libde265/libde265/Release/de265.lib",
              "-DX265_INCLUDE_DIR=" + src_dir + "/x265_git/source",
              "-DX265_LIBRARY=" + src_dir + "/x265_git/build/Release/libx265.lib",
-             "-DWITH_X265=yes", "--preset=release"])
+             "-DWITH_X265_PLUGIN=ON", "-DENABLE_PLUGIN_LOADING=ON","--preset=release"])
+        base.cmd("cmake", ["--build", ".", "--config", "Release"])
+    if "linux" == base.host_platform():
+        base.cmd("cmake", ["../../../", "-DCMAKE_BUILD_TYPE=Release", 
+             "-DLIBDE265_INCLUDE_DIR=" + src_dir + "/libde265",
+             "-DLIBDE265_LIBRARY=" + src_dir + "/libde265/libde265/libde265.so",
+             "-DX265_INCLUDE_DIR=" + src_dir + "/x265_git/source",
+             "-DX265_LIBRARY=" + src_dir + "/x265_git/build/libx265.so",
+             "-DWITH_X265_PLUGIN=ON", "-DENABLE_PLUGIN_LOADING=ON","--preset=release"])
+        base.cmd("make", ["-j$(nproc)"])
+    if "mac" == base.host_platform():
+        base.cmd("cmake", ["../../../", "-DCMAKE_BUILD_TYPE=Release", 
+             "-DLIBDE265_INCLUDE_DIR=" + src_dir + "/libde265",
+             "-DLIBDE265_LIBRARY=" + src_dir + "/libde265/libde265/libde265.dylib",
+             "-DX265_INCLUDE_DIR=" + src_dir + "/x265_git/source",
+             "-DX265_LIBRARY=" + src_dir + "/x265_git/build/libx265.dylib",
+             "-DWITH_X265_PLUGIN=ON", "-DENABLE_PLUGIN_LOADING=ON","--preset=release"])
+        base.cmd("make", ["-j$(sysctl -n hw.ncpu)"])
+    
     base.cmd("cmake", ["--build", ".", "--config", "Release"])
     base.copy_file(build_dir + "/libheif/heif_version.h", build_dir)
     os.chdir(src_dir)
@@ -55,3 +87,6 @@ def make():
 
     os.chdir(old_dir)
     return
+
+if __name__ == '__main__':
+    make()

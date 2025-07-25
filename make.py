a0cc2123bf678a134bc0dc13deca6db0c2a9b942
Maxim Kadushkin <maxim.kadushkin@onlyoffice.com>
2025-07-25 15:08:51 +0000

    Update make.py

--- make.py
+++ make.py
@@ -87,8 +87,6 @@ make_common.make()
 # build updmodule for desktop (only for windows version)
 if config.check_option("module", "desktop"):
   config.extend_option("qmake_addon", "URL_WEBAPPS_HELP=https://download.onlyoffice.com/install/desktop/editors/help/v" + base.get_env('PRODUCT_VERSION') + "/apps")
-  # config.extend_option("qmake_addon", "CFG_APP_LICENSE_NAME=\"\\\"ASDFG v4\\\"\"")
-  # config.extend_option("qmake_addon", "CFG_APP_LICENSE_URL=\"\\\"https://onlyoffice.com\\\"\"")
 
   if "windows" == base.host_platform():
     config.extend_option("config", "updmodule")

4f09833650f87f4e051ef3e391eeffe9fc03c599
Mikhail Lobotskiy <Mikhail.Lobotskiy@onlyoffice.com>
2025-08-12 20:55:47 +0400

    Fix win builds
    and refactoring

--- scripts/core_common/modules/heif.py
+++ scripts/core_common/modules/heif.py
@@ -4,6 +4,15 @@ import base
 import os
 import config
 
+# NOTE:
+#  - requires CMake < 4.0.0
+
+def get_vs_version():
+  vs_version = "14 2015"
+  if config.option("vs-version") == "2019":
+    vs_version = "16 2019"
+  return vs_version
+
 def make_x265(base_dir, build_type):
   # clones repo
   def fetch(version):
@@ -11,7 +20,7 @@ def make_x265(base_dir, build_type):
     base.cmd("git", ["clone", "--depth", "1", "--branch", branch_name, "https://bitbucket.org/multicoreware/x265_git.git"])
     return
 
-  # builds (Unix only)
+  # builds library (Unix specific)
   def build_unix(platform, cmake_args):
     # check build dir
     build_dir = os.path.join(base_dir, "x265_git/build", platform, build_type.lower())
@@ -30,10 +39,35 @@ def make_x265(base_dir, build_type):
     os.chdir(old_dir)
     return
 
+  # builds library (Windows specific)
+  def build_win(platform, cmake_args):
+    # check build dir
+    build_dir = os.path.join(base_dir, "x265_git/build", platform, build_type.lower())
+    if base.is_file(os.path.join(build_dir, build_type, "x265-static.lib")):
+      return
+    base.create_dir(build_dir)
+    # go to build dir
+    old_dir = os.getcwd()
+    os.chdir(build_dir)
+    # build
+    base.cmd("cmake", cmake_args)
+    base.cmd("cmake", ["--build", ".", "--config", build_type])
+    # copy files
+    base.copy_file(base_dir + "/x265_git/source/x265.h", build_dir)
+    # restore old dir
+    os.chdir(old_dir)
+    return
+
   # set version here
   x265_version = "4.1"
   if not base.is_dir("x265_git"):
     fetch(x265_version)
+    # fix x265 version detection so it reads version from x265Version.txt instead of parsing it from .git
+    base.replaceInFile(
+      base_dir + "/x265_git/source/cmake/Version.cmake",
+      "elseif(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../x265Version.txt)",
+      "endif()\n    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../x265Version.txt)"
+    )
 
   cmake_dir = base_dir + "/x265_git/source"
   cmake_args = [
@@ -43,12 +77,19 @@ def make_x265(base_dir, build_type):
     "-DENABLE_SHARED=OFF"               # do not build shared libs
   ]
 
-  # if "windows" == base.host_platform():
-  #   # TODO: separate win32 and win64 builds
-  #   cmake_args.extend(["-G", "Visual Studio 16 2019", "-A", "x64"])
-  #   base.cmd("cmake", cmake_args)
-  #   base.cmd("cmake", ["--build", ".", "--config", build_type])
-  #   base.copy_files(cmake_dir + "/" + build_type + "/*.lib", lib_dir)
+  # WINDOWS
+  if "windows" == base.host_platform():
+    cmake_args += [
+      "-G", f"Visual Studio {get_vs_version()}"
+    ]
+    # win_64
+    if config.check_option("platform", "win_64"):
+      cmake_args_ext = ["-A", "x64"]
+      build_win("win_64", cmake_args + cmake_args_ext)
+    # win_32
+    if config.check_option("platform", "win_32"):
+      cmake_args_ext = ["-A", "Win32"]
+      build_win("win_32", cmake_args + cmake_args_ext)
 
   # LINUX
   if "linux" == base.host_platform():
@@ -56,11 +97,9 @@ def make_x265(base_dir, build_type):
       "-G", "Unix Makefiles",
       "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
     ]
-
     # linux_64
     if config.check_option("platform", "linux_64"):
       build_unix("linux_64", cmake_args)
-
     # linux_arm64
     if config.check_option("platform", "linux_arm64"):
       build_unix("linux_arm64", cmake_args)
@@ -72,7 +111,6 @@ def make_x265(base_dir, build_type):
       "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
       "-DENABLE_ASSEMBLY=OFF"                   # disable assembly optimizations
     ]
-
     # mac_64
     if config.check_option("platform", "mac_64"):
       cmake_args_ext = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11"]
@@ -81,7 +119,6 @@ def make_x265(base_dir, build_type):
           "-DCMAKE_OSX_ARCHITECTURES=x86_64"
         ]
       build_unix("mac_64", cmake_args + cmake_args_ext)
-
     # mac_arm64
     if config.check_option("platform", "mac_arm64"):
       cmake_args_ext = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=11.0"]
@@ -97,7 +134,7 @@ def make_de265(base_dir, build_type):
     base.cmd("git", ["clone", "--depth", "1", "--branch", tag_name, "https://github.com/strukturag/libde265.git"])
     return
 
-  # builds (Unix only)
+  # builds library (Unix specific)
   def build_unix(platform, cmake_args):
     # prepare build dir
     build_dir = os.path.join(base_dir, "libde265/build", platform, build_type.lower())
@@ -116,6 +153,25 @@ def make_de265(base_dir, build_type):
     os.chdir(old_dir)
     return
 
+  # builds library (Windows specific)
+  def build_win(platform, cmake_args):
+    # check build dir
+    build_dir = os.path.join(base_dir, "libde265/build", platform, build_type.lower())
+    if base.is_file(os.path.join(build_dir, "libde265", build_type, "libde265.lib")):
+      return
+    base.create_dir(build_dir)
+    # go to build dir
+    old_dir = os.getcwd()
+    os.chdir(build_dir)
+    # build
+    base.cmd("cmake", cmake_args)
+    base.cmd("cmake", ["--build", ".", "--config", build_type])
+    # copy files
+    base.copy_file(base_dir + "/libde265/libde265/de265.h", build_dir + "/libde265")
+    # restore old dir
+    os.chdir(old_dir)
+    return
+
   # set version here
   de265_version = "1.0.16"
   if not base.is_dir("libde265"):
@@ -127,15 +183,23 @@ def make_de265(base_dir, build_type):
     "-DCMAKE_BUILD_TYPE=" + build_type,
     "-DBUILD_SHARED_LIBS=OFF",            # do not build shared libs
     "-DENABLE_SDL=OFF",                   # disable SDL
-    "-DENABLE_ENCODER=OFF",               # disable encoder (we use x265 for that)
+    "-DENABLE_DECODER=OFF",               # do not build decoder CLI executable 
+    "-DENABLE_ENCODER=OFF",               # do not build encoder CLI executable
   ]
 
-  # if "windows" == base.host_platform():
-  #   base.cmd("cmake", cmake_args)
-  #   base.cmd("cmake", ["--build", ".", "--config", build_type])
-  #   base.copy_files(cmake_dir + "/libde265/" + build_type + "/*.lib", lib_dir)
-
-  # TODO: do we need -DCMAKE_POSITION_INDEPENDENT_CODE=ON on Unix ???
+  # WINDOWS
+  if "windows" == base.host_platform():
+    cmake_args += [
+      "-G", f"Visual Studio {get_vs_version()}"
+    ]
+    # win_64
+    if config.check_option("platform", "win_64"):
+      cmake_args_ext = ["-A", "x64"]
+      build_win("win_64", cmake_args + cmake_args_ext)
+    # win_32
+    if config.check_option("platform", "win_32"):
+      cmake_args_ext = ["-A", "Win32"]
+      build_win("win_32", cmake_args + cmake_args_ext)
 
   # LINUX
   if "linux" == base.host_platform():
@@ -143,11 +207,9 @@ def make_de265(base_dir, build_type):
       "-G", "Unix Makefiles",
       "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
     ]
-
     # linux_64
     if config.check_option("platform", "linux_64"):
       build_unix("linux_64", cmake_args)
-
     # linux_arm64
     if config.check_option("platform", "linux_arm64"):
       build_unix("linux_arm64", cmake_args)
@@ -158,7 +220,6 @@ def make_de265(base_dir, build_type):
       "-G", "Unix Makefiles",
       "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"
     ]
-
     # mac_64
     if config.check_option("platform", "mac_64"):
       cmake_args_ext = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11"]
@@ -167,7 +228,6 @@ def make_de265(base_dir, build_type):
           "-DCMAKE_OSX_ARCHITECTURES=x86_64"
         ]
       build_unix("mac_64", cmake_args + cmake_args_ext)
-
     # mac_arm64
     if config.check_option("platform", "mac_arm64"):
       cmake_args_ext = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=11.0"]
@@ -183,7 +243,7 @@ def make_heif(base_dir, build_type):
     base.cmd("git", ["clone", "--depth", "1", "--branch", tag_name, "https://github.com/strukturag/libheif.git"])
     return
 
-  # builds (Unix only)
+  # builds library (Unix specific)
   def build_unix(platform, cmake_args):
     # prepare build dir
     build_dir = os.path.join(base_dir, "libheif/build", platform, build_type.lower())
@@ -205,6 +265,32 @@ def make_heif(base_dir, build_type):
     # build
     base.cmd("cmake", cmake_args + cmake_args_ext)
     base.cmd("make", ["-j4"])
+    # restore old dir
+    os.chdir(old_dir)
+    return
+
+  # builds library (Windows specific)
+  def build_win(platform, cmake_args):
+    # check build dir
+    build_dir = os.path.join(base_dir, "libheif/build", platform, build_type.lower())
+    if base.is_file(os.path.join(build_dir, "libheif", build_type, "heif.lib")):
+      return
+    base.create_dir(build_dir)
+    # go to build dir
+    old_dir = os.getcwd()
+    os.chdir(build_dir)
+    # add paths to dependent libraries and includes to cmake args
+    de265_build_dir = os.path.join(base_dir, "libde265/build", platform, build_type.lower())
+    x265_build_dir = os.path.join(base_dir, "x265_git/build", platform, build_type.lower())
+    cmake_args_ext = [
+      f"-DLIBDE265_INCLUDE_DIR={de265_build_dir}",
+      f"-DLIBDE265_LIBRARY={de265_build_dir}/libde265/{build_type}/libde265.lib",
+      f"-DX265_INCLUDE_DIR={x265_build_dir}",
+      f"-DX265_LIBRARY={x265_build_dir}/{build_type}/x265-static.lib"
+    ]
+    # build
+    base.cmd("cmake", cmake_args + cmake_args_ext)
+    base.cmd("cmake", ["--build", ".", "--config", build_type])
     # restore old dir
     os.chdir(old_dir)
     return
@@ -237,25 +323,34 @@ def make_heif(base_dir, build_type):
     "-DCMAKE_C_FLAGS=-DLIBDE265_STATIC_BUILD",        # ^
   ]
 
-  # if "windows" == base.host_platform():
-  #   cmake_args.extend(["-DLIBDE265_INCLUDE_DIR=" + src_dir + "/libde265",
-  #              "-DLIBDE265_LIBRARY=" + lib_dir + "/libde265.lib",
-  #              "-DX265_INCLUDE_DIR=" + src_dir + "/x265_git/source",
-  #              "-DX265_LIBRARY=" + lib_dir + "/x265-static.lib",])
-  #   base.cmd("cmake", cmake_args)
-  #   base.cmd("cmake", ["--build", ".", "--config", build_type])
-  #   base.copy_files(cmake_dir + "/libheif/" + build_type + "/*.lib", lib_dir)
+  # WINDOWS
+  if "windows" == base.host_platform():
+    cmake_args += [
+      "-G", f"Visual Studio {get_vs_version()}"
+    ]
+    # win_64
+    if config.check_option("platform", "win_64"):
+      cmake_args_ext = ["-A", "x64"]
+      build_win("win_64", cmake_args + cmake_args_ext)
+    # win_32
+    if config.check_option("platform", "win_32"):
+      cmake_args_ext = ["-A", "Win32"]
+      build_win("win_32", cmake_args + cmake_args_ext)
 
-  # if "linux" == base.host_platform():
-  #   cmake_args.extend(["-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
-  #              "-DWITH_GDK_PIXBUF=OFF", "-DWITH_GNOME=OFF",
-  #              "-DLIBDE265_INCLUDE_DIR=" + src_dir + "/libde265",
-  #              "-DLIBDE265_LIBRARY=" + lib_dir + "/libde265.a",
-  #              "-DX265_INCLUDE_DIR=" + src_dir + "/x265_git/source",
-  #              "-DX265_LIBRARY=" + lib_dir + "/libx265.a"])
-  #   base.cmd("cmake", cmake_args)
-  #   base.cmd("make", ["-j$(nproc)"])
-  #   base.copy_files(cmake_dir + "/libheif/*.a", lib_dir)
+  # LINUX
+  if "linux" == base.host_platform():
+    cmake_args += [
+      "-G", "Unix Makefiles",
+      "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+      "-DWITH_GDK_PIXBUF=OFF",                  # do not build gdk-pixbuf plugin
+      "-DWITH_GNOME=OFF",                       # do not build gnome plugin
+    ]
+    # linux_64
+    if config.check_option("platform", "linux_64"):
+      build_unix("linux_64", cmake_args)
+    # linux_arm64
+    if config.check_option("platform", "linux_arm64"):
+      build_unix("linux_arm64", cmake_args)
 
   # MAC
   if "mac" == base.host_platform():
@@ -264,7 +359,6 @@ def make_heif(base_dir, build_type):
       "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
       "-DWITH_GDK_PIXBUF=OFF",                  # do not build gdk-pixbuf plugin
     ]
-
     # mac_64
     if config.check_option("platform", "mac_64"):
       cmake_args_ext = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=10.11"]
@@ -273,7 +367,6 @@ def make_heif(base_dir, build_type):
           "-DCMAKE_OSX_ARCHITECTURES=x86_64"
         ]
       build_unix("mac_64", cmake_args + cmake_args_ext)
-
     # mac_arm64
     if config.check_option("platform", "mac_arm64"):
       cmake_args_ext = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=11.0"]

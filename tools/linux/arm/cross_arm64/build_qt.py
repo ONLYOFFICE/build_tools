a0c4dc053cc73aae06ebbf4aecfa640bb50b6df4
Alexey Nagaev <aleksei.nagaev@onlyoffice.com>
2025-08-21 04:13:40 +0300

    Build glib for linux arm64
    But getting error

--- tools/linux/arm/cross_arm64/build_qt.py
+++ tools/linux/arm/cross_arm64/build_qt.py
@@ -6,9 +6,65 @@ sys.path.append('../../../../scripts')
 
 import base
 
-def update_qmake_conf(arm_toolchain_bin):
+# return data for cross_file (meson)
+def get_cross_file_glib_data(arm_toolchain_path) -> str:
+  arm_toolchain_bin = arm_toolchain_path + "/bin/"
+  arm_toolchain_prefix = "aarch64-linux-gnu-"
+  
+  cross_file_data = ""
+  cross_file_data += "[host_machine]\n"
+  cross_file_data += "system = 'linux'\n"
+  cross_file_data += "cpu_family = 'aarch64'\n"
+  cross_file_data += "cpu = 'aarch64'\n"
+  cross_file_data += "endian = 'little'\n"
+  cross_file_data += "[properties]\n"
+  cross_file_data += "c_args = []\n"
+  cross_file_data += "c_link_args = []\n"
+  cross_file_data += "[binaries]\n"
+  cross_file_data += "c = '" + arm_toolchain_bin + arm_toolchain_prefix + "gcc'\n"
+  cross_file_data += "cpp = '" + arm_toolchain_bin + arm_toolchain_prefix +"g++'\n"
+  cross_file_data += "ar = '" + arm_toolchain_bin + arm_toolchain_prefix + "ar'\n"
+  cross_file_data += "ld = '" + arm_toolchain_bin + arm_toolchain_prefix + "ld'\n"
+  cross_file_data += "objcopy = '" + arm_toolchain_bin + arm_toolchain_prefix + "/objcopy'\n"
+  cross_file_data += "strip = '" + arm_toolchain_bin + arm_toolchain_prefix + "strip'\n"
+  return cross_file_data
+  
+# return builded glib
+def build_glib(arm_toolchain_path) -> str:
+  curr_dir = os.path.abspath(os.curdir)
+  build_path = os.path.abspath("./glib-build")
+  install_path = os.path.abspath("./glib-install")
+  glib_path = os.path.abspath("./glib")
+  
+  if base.is_dir(install_path):
+    return install_path
+  
+  if not base.is_dir("./glib"):
+    base.cmd("git clone " + "https://gitlab.gnome.org/GNOME/glib.git")
+    
+  base.cmd_in_dir("glib", "git checkout 2.82.2")
+  
+  if not base.is_dir(build_path):
+    base.cmd("mkdir " + build_path)
+    
+  os.chdir(build_path)
+  
+  cross_filename = "cross_file.txt"
+  cross_file_data = get_cross_file_glib_data(arm_toolchain_path)
+  base.writeFile(cross_filename, cross_file_data)
+  
+  base.cmd("pip install meson")
+  base.cmd("meson setup " + glib_path + " -Dbuildtype=release --prefix " + install_path + " --cross-file " + cross_filename)
+  base.cmd("meson compile")
+  base.cmd("meson install")
+  
+  os.chdir(curr_dir)
+  
+  return install_path
+  
+def update_qmake_conf(arm_toolchain_path):
   replace_file = "./qt-everywhere-src-5.15.2/qtbase/mkspecs/linux-aarch64-gnu-g++/qmake.conf"
-  arm_toolchain_bin = os.path.abspath(arm_toolchain_bin)
+  arm_toolchain_bin = arm_toolchain_path + "/bin"
   
   replace_src = ""
   replace_src += "# modifications to g++.conf\n"
@@ -38,7 +94,15 @@ def update_qmake_conf(arm_toolchain_bin):
   
   base.replaceInFile(replace_file, replace_src, replace_dst)
 
-def make(arm_toolchain_bin=""):
+def make(arm_toolchain_path="", arm_sysroot_path=""):
+  arm_toolchain_path = os.path.abspath(arm_toolchain_path)
+  arm_sysroot_path = os.path.abspath(arm_sysroot_path)
+  
+  glib_install_path = build_glib(arm_toolchain_path)
+  glib_lib = glib_install_path + "/lib"
+  glib_include = glib_install_path + "/include"
+
+  
   qt_build_path = os.path.dirname(os.path.abspath(__file__)) + "/qt_build/Qt-5.15.2/linux_arm64"
   qt_params = ["-opensource",
     "-confirm-license",
@@ -54,8 +118,7 @@ def make(arm_toolchain_bin=""):
     "-qt-libjpeg",
     "-qt-pcre",
     "-glib",
-    "-xcb",
-    "-xcb-xlib",
+ #   "-xcb",
     "-no-sql-sqlite",
     "-no-opengl",
     "-nomake", "examples",
@@ -69,8 +132,12 @@ def make(arm_toolchain_bin=""):
     "-skip", "qtwebengine",
     "-skip", "qtdeclarative",
     "-xplatform", "linux-aarch64-gnu-g++", # be sure that aarch64 gnu compiler is installed
-    "-no-pch"]
-
+    "-no-pch",
+    "-no-use-gold-linker",
+    "-sysroot " + arm_sysroot_path,
+    "-L " + glib_lib,
+    "-I " + glib_include]
+  
   qt_params_str = ""
   for param in qt_params:
       qt_params_str += (param + " ")
@@ -91,16 +158,19 @@ def make(arm_toolchain_bin=""):
     filedata = additional_gcc_11 + filedata
     base.writeFile(chanage_file, filedata)
     
-  if arm_toolchain_bin != "":
-    update_qmake_conf(arm_toolchain_bin)
+  if arm_toolchain_path != "":
+    update_qmake_conf(arm_toolchain_path)
 
   base.cmd_in_dir("./qt-everywhere-src-5.15.2", "./configure " + qt_params_str)
   base.cmd_in_dir("./qt-everywhere-src-5.15.2", "make -j4")
   base.cmd_in_dir("./qt-everywhere-src-5.15.2", "make install")
   
 if __name__ == "__main__":
-  arm_toolchain_path = "./gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu/bin"
-  if len(sys.argv) != 1:
+  arm_toolchain_path = "./gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu"
+  arm_sysroot_path = "./sysroot-glibc-linaro-2.21-2017.05-aarch64-linux-gnu"
+
+  if len(sys.argv) >= 3:
     arm_toolchain_path = sys.argv[1]
+    arm_sysroot_path = sys.argv[2]
     
-  make(arm_toolchain_path)
+  make(arm_toolchain_path, arm_sysroot_path)
--- tools/linux/arm/cross_arm64/download_arm_toolchain.py
+++ tools/linux/arm/cross_arm64/download_arm_toolchain.py
@@ -7,10 +7,25 @@ sys.path.append('../../../../scripts')
 import base
 
 def make():
-    arm_toolchain_url = 'https://releases.linaro.org/components/toolchain/binaries/5.4-2017.05/aarch64-linux-gnu/'
+    arm_url = 'https://releases.linaro.org/components/toolchain/binaries/5.4-2017.05/aarch64-linux-gnu/'
+    
     arm_toolchain_tar_filename = 'gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz'
-    base.cmd2('wget', [arm_toolchain_url + arm_toolchain_tar_filename])
-    base.cmd2('tar', ['-xf', arm_toolchain_tar_filename])
+    arm_toolchain_dirname = 'gcc-linaro-5.4.1-2017.05-x86_64_aarch64-linux-gnu'
+    
+    arm_sysroot_tar_filename = 'sysroot-glibc-linaro-2.21-2017.05-aarch64-linux-gnu.tar.xz'
+    arm_sysroot_dirname = 'sysroot-glibc-linaro-2.21-2017.05-aarch64-linux-gnu'  
+    
+    if not base.is_file(arm_toolchain_tar_filename):
+        base.cmd2('wget', [arm_url + arm_toolchain_tar_filename])
+    
+    if not base.is_dir(arm_toolchain_dirname):
+        base.cmd2('tar', ['-xf', arm_toolchain_tar_filename])
+        
+    if not base.is_file(arm_sysroot_tar_filename):
+        base.cmd2('wget', [arm_url + arm_sysroot_tar_filename])
+    
+    if not base.is_dir(arm_sysroot_dirname):
+        base.cmd2('tar', ['-xf', arm_sysroot_tar_filename])
 
 if __name__ == "__main__":
 	make()

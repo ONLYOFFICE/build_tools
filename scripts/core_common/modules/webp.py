089bba3b8405d5739c5e2d510d84bfc3c1e1cef8
Prokhorov Kirill <kirill.prohorov@onlyoffice.com>
2025-10-14 14:36:38 +0300

    Fix libwebp build for liux

--- scripts/core_common/modules/webp.py
+++ scripts/core_common/modules/webp.py
@@ -17,16 +17,17 @@ def make():
     base_dir = base_dir + "/libwebp"
     os.chdir(base_dir)
 
-    build_type = "release-static"
+    build_type = "release"
     target_file = "/libwebp"
     if (-1 != config.option("config").lower().find("debug")):
-        build_type = "debug-static"
+        build_type = "debug"
         target_file = "/libwebp-debug"
 
     # WINDOWS
     if "windows" == base.host_platform():
         platform = "win_64"
         arch = "x64"
+        build_type = build_type + "-static"
         make_bat = []
 
         if config.check_option("platform", "win_32"):
@@ -49,19 +50,26 @@ def make():
     elif "linux" == base.host_platform():
         platform = "linux_64"
         arch = "x86_64"
+        target_file = "libwebp.a"
+        make_args = ["-f", "makefile.unix", target_file]
+
+        debug = ""
+        if build_type == "debug":
+            debug = " -g "
 
         if config.check_option("platform", "linux_arm64"):
             platform = "linux_arm64"
             arch = "armv8-a"
 
-        build_dir = "./../build/" + platform
+        build_dir = "./../build/" + platform + "/" + build_type
+        make_args.append("CFLAGS=\"" + debug + "-march=" + arch + "\"")
         if not base.is_dir(build_dir):
             base.create_dir(build_dir)
-        if base.is_file(build_dir + "/.libs" + target_file + ".a"):
+        if base.is_file(build_dir + "/" + target_file):
             return
 
-        base.cmd("make", ["-f", "makefile.unix", "CFLAGS=\" -march=" + arch,
-                          "TARGET_DIR=$(pwd)" + build_dir])
+        base.cmd("make", make_args)
+        base.copy_file(target_file, build_dir)
 
     os.chdir(old_dir)
     return
